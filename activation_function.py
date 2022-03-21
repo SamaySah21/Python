e4ba2571db58168c9ea7d9cf024581e9219be9b7
SamaySah21 <samaysah21@gmail.com>
2022-03-21 13:54:10 +0530

    sigmoid ,tanh and relu

--- activation_function.py
+++ activation_function.py
@@ -18,5 +18,34 @@ output = [rectifiedLinearUnit(x) for x in inputs]
 
 print(output)
 # plot input vs output
-pyplot.plot(inputs, output)
-pyplot.show()
+# pyplot.plot(inputs, output)
+# pyplot.show()
+
+########## SIGMOID FUNCTION ###############3
+
+# output varies in between 0 to 1
+#The larger the input (more positive), the closer the output value will be to 1.0,
+# the smaller the input (more negative), the closer the output will be to 0.0
+
+from math import exp
+def sigmoid(x):
+    #exp(-x) is same as pow(2.71828, x) as exp is euler number whose value is equal to 2.718
+    return 1.0 / (1 + exp(-x))
+
+# there are 2 problem with sigmoid function
+# 1. vanishing gradient (same in tanh also)
+# 2. value which are smaller are getting value close to zero , there is loss of all negative values (fixed using tanh)
+
+#################### Tanh function ########################
+
+# output values between -1 to +!
+# The larger the input (more positive), the closer the output value will be to 1.0,
+# the smaller the input (more negative), the closer the output will be to -1.0.
+def tanFunction(x):
+    return (exp(x) - exp(-x)) / (exp(x) + exp(-x))
+
+
+
+
+
+
